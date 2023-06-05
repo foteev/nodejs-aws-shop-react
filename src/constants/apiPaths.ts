e5d6d342acb7b5b0c5f4a40ff57e52f808a9ea1e
vivi196 <foteev@gmail.com>
2023-06-06 00:35:23 +0200

    feat: add realdata url nd paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-central-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-central-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://lbetr6wm97.execute-api.eu-central-1.amazonaws.com",
   cart: "https://.execute-api.eu-central-1.amazonaws.com/dev",
 };
 
